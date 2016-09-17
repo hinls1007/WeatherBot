f2f3175a88f2f1f0c43f10d32a8c3384a2518690
Hin Lai <h.lai@actimirror.com>
2016-09-17 16:10:42 +0800

    - Add Subscribe and unsubscribe function - Adding JavaDoc

--- src/main/java/com/hinl/weatherbot/Main.java
+++ src/main/java/com/hinl/weatherbot/Main.java
@@ -3,6 +3,8 @@ package com.hinl.weatherbot;
 import com.hinl.weatherbot.Handler.ChangeLanguageHandler;
 import com.hinl.weatherbot.Handler.CurrentWeatherHandler;
 import com.hinl.weatherbot.Handler.WarningHandler;
+import com.hinl.weatherbot.Utils.CheckUpdateThread;
+import com.hinl.weatherbot.Utils.Const;
 import com.hinl.weatherbot.Utils.DBHelper;
 import com.hinl.weatherbot.receiver.BotMessasgeReciever;
 import org.telegram.telegrambots.TelegramApiException;
@@ -16,13 +18,33 @@ public class Main {
 
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         DBHelper.init();
-        BotMessasgeReciever.registerHandler(CurrentWeatherHandler.class);
-        BotMessasgeReciever.registerHandler(WarningHandler.class);
-        BotMessasgeReciever.registerHiddenHandler(ChangeLanguageHandler.class);
+        registerHandler();
+        startTrackingThread();
         try {
             telegramBotsApi.registerBot(new BotMessasgeReciever());
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     *  Start Tracking Thread for polling update the warning and current weather
+     */
+
+    public static void startTrackingThread(){
+        CheckUpdateThread.addTracking(Const.CurrentWeather_EN, "current");
+        CheckUpdateThread.addTracking(Const.WeatherWarning_EN, "warning");
+        CheckUpdateThread thread = new CheckUpdateThread();
+        thread.start();
+    }
+
+    /**
+     * register the command Handler for different command and topics
+     */
+    public static void registerHandler(){
+
+        BotMessasgeReciever.registerHandler(CurrentWeatherHandler.class);
+        BotMessasgeReciever.registerHandler(WarningHandler.class);
+        BotMessasgeReciever.registerHiddenHandler(ChangeLanguageHandler.class);
     }
 }
